de1ce740697618f1f2a57183d2846625a59a4f72
Adrian Pauli <pauli.adrian96@gmail.com>
2019-05-21 15:07:19 +0200

    Add initial project

---
+++ src/main/java/com/github/homespirit/httpserialcontrol/HttpSerialControlApplication.java
@@ -0,0 +1,13 @@
+package com.github.homespirit.httpserialcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpSerialControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpSerialControlApplication.class, args);
+	}
+
+}
